23bfc8e7526ba83ef9eb4888eb394243e510dfaa
daredevil555 <46808885+daredevil555@users.noreply.github.com>
2020-03-02 21:17:50 +0530

    Add files via upload

--- models/student.js
+++ models/student.js
@@ -4,11 +4,19 @@ var Schema = mongoose.Schema;
 
 var studentSchema = new Schema({
 
-  student_name: { type: String, Required:  'student name cannot be left blank.' },
+  email: {type:String,},
+  question1: { type: String, },
+  question2: { type: String, },
+  question3: { type: String, },
+  question4: { type: String, },
+  question5: { type: String, },
+  question6: { type: String, },
+  question7: { type: String, },
+  question8: { type: String, },
+  question9: { type: String, },
+  question10: { type: String, },
 
-  branch:    { type: String,     Required:  'student branch cannot be left blank.'},
 
-  interest: { type: String ,    Required:  'student intrest cannot be left blank'}
 
 });
 
