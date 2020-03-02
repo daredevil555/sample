3755d2b5723e592dfe4d175ecbcd2fc8c8fab58a
daredevil555 <46808885+daredevil555@users.noreply.github.com>
2020-03-02 21:14:42 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,7 @@ var student = require('../models/student');
 //  });
 
 // connecting to mlab
-
-const uri = "mongodb+srv://Eshwar:ani4anirudh1999%23@cluster-info-rm5w6.mongodb.net/csv?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Eshwar:ani4anirudh1999%23@cluster-info-rm5w6.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(() => console.log(`Connected to mlab..!!`))
@@ -34,16 +33,29 @@ router.get('/form',function(req,res,next){
 // POST to save the details of the user
 router.post('/saveform',function(req,res,next){
   var data = {
-    student_name:req.body.name,
-    branch:req.body.branch,
-    interest:req.body.interest
+    email:req.body.email,
+    question1:req.body.optq1,
+    question2:req.body.optq2,
+    question3:req.body.optq3,
+    question4:req.body.optq4,
+    question5:req.body.optq5,
+    question6:req.body.optq6,
+    question7:req.body.optq7,
+    question8:req.body.optq8,
+    question9:req.body.optq9,
+    question10:req.body.optq10,
   }
 
  student.create(data,function(err,result){
   if(err) console.log(err);
   console.log("Inserted sucessfully");
 });
-  res.render('form');
+  res.redirect('/thankyou');
+});
+
+// GET for result
+router.get('/thankyou',function(req,res){
+  res.render('result');
 });
 
 // GET to show all the students details
@@ -61,11 +73,9 @@ router.get('/tocsv',function(req,res,next){
 
 router.get('/exportcsv', function(req, res, next) {
 
-  var filename   = "products.csv";
+  var filename   = "data.csv";
 
-  var dataArray;
-
-  student.find().lean().exec({}, function(err, products) {
+  student.find().lean().exec({}, function(err, data) {
 
       if (err) res.send(err);
       
@@ -75,7 +85,7 @@ router.get('/exportcsv', function(req, res, next) {
 
       res.setHeader("Content-Disposition", 'attachment; filename='+filename);
 
-      res.csv(products, true);
+      res.csv(data, true);
 
   });
 
