9e486aee6c1bc15734b79b4b1a520a6b9a13f85b
daredevil555 <46808885+daredevil555@users.noreply.github.com>
2020-03-04 23:51:58 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var csv = require('csv-express');
 var mongoose = require('mongoose');
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
 var student = require('../models/student');
@@ -8,26 +9,31 @@ var student = require('../models/student');
 // connecting to database #mongodb
 // let url = process.env.DATABASEURL || "mongodb://localhost/csv";
 //  mongoose.connect(url, { useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true },function(err,database){
-//     db=database;
 //  });
+
+// code for nodemailer
+var smtpTransport = nodemailer.createTransport({
+  service: 'Gmail', 
+  auth: {
+    user: 'mrcetclub@gmail.com',
+    pass: '#thedarkangels'
+  }
+});
 
 // connecting to mlab
 const uri = "mongodb+srv://Eshwar:ani4anirudh1999%23@cluster-info-rm5w6.mongodb.net/test?retryWrites=true&w=majority";
-
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(() => console.log(`Connected to mlab..!!`))
 .catch(err => console.log(`Database connection error: ${err.message}`));
 
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-// GET to obtain the form page
-router.get('/form',function(req,res,next){
-  res.render('form');
+// GET for responded
+router.get('/responded',function(req,res){
+  res.render('responded');
 });
 
 // POST to save the details of the user
@@ -46,11 +52,32 @@ router.post('/saveform',function(req,res,next){
     question10:req.body.optq10,
   }
 
- student.create(data,function(err,result){
-  if(err) console.log(err);
-  console.log("Inserted sucessfully");
-});
-  res.redirect('/thankyou');
+  student.findOne({email:data.email},function(err,result){
+    if(err)
+    console.log(err);
+    if(result){
+    res.redirect('/responded');
+    }
+    else{
+       student.create(data,function(err,result){
+          if(err) console.log(err);
+          console.log("Inserted sucessfully");
+          // Mail options to send mail
+          var mailOptions = {
+            to: data.email,
+            from: 'mrcetclub@mail.com',
+            subject: 'Find Your Twin',
+            text: 'Hey, Folk \n' +
+            'This is a confirmation that your response is recorded associated with this email. \nThank You team findyourtwin' 
+          };
+          smtpTransport.sendMail(mailOptions, function(err) {
+            console.log('mail sent');
+          });
+        res.redirect('/thankyou');
+        });
+      }
+  });
+
 });
 
 // GET for result
@@ -66,27 +93,20 @@ router.get('/tocsv',function(req,res,next){
     if (err)
     console.log(err);
 
-    res.render('mainpage', {list: products });
+    res.render('data', {list: products });
 
 });
 });
 
+// GET to export csv file
 router.get('/exportcsv', function(req, res, next) {
-
   var filename   = "data.csv";
-
   student.find().lean().exec({}, function(err, data) {
-
       if (err) res.send(err);
-      
       res.statusCode = 200;
-
       res.setHeader('Content-Type', 'text/csv');
-
       res.setHeader("Content-Disposition", 'attachment; filename='+filename);
-
       res.csv(data, true);
-
   });
 
 });
